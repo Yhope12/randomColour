73dcf89b81d79e516b1c2eaebc952f57f3961fba
Yhope12 <139056953+Yhope12@users.noreply.github.com>
2023-08-13 21:37:50 +0630

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,10 +1,24 @@
 const colourBtn = document.getElementById("colour");
 const body = document.getElementById("body");
+const h2 = document.querySelector("h2");
 
-colourBtn.addEventListener("click", () => {
+function change() {
   let x = Math.ceil(Math.random() * 256);
   let y = Math.ceil(Math.random() * 256);
   let z = Math.ceil(Math.random() * 256);
-  body.style.backgroundColor = `rgb(${x},${y},${z})`;
-  console.log(`rgb(${x},${y},${z})`);
-});
+  let a = Math.random();
+  return `rgb(${x},${y},${z},${a})`;
+}
+
+setInterval(() => {
+  document.body.style.backgroundColor = change();
+  h2.innerHTML = change();
+}, 1000);
+
+// colourBtn.addEventListener("click", () => {
+//   let x = Math.ceil(Math.random() * 256);
+//   let y = Math.ceil(Math.random() * 256);
+//   let z = Math.ceil(Math.random() * 256);
+//   body.style.backgroundColor = `rgb(${x},${y},${z})`;
+//   console.log(`rgb(${x},${y},${z})`);
+// });
